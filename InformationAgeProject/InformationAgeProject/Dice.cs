c6e1040d4d50258747d851428c382031ed88764a
BobbyDMullinsIII <61435002+BobbyDMullinsIII@users.noreply.github.com>
2021-09-20 22:52:49 -0400

    Added RollDice(), Removed StaticRollDice()
    +Added RollDice() method for rolling any number of regular 6-sided dice
    -Removed StaticRollDice() method because it had to use a new RNG every cycle and that returned the same number

--- InformationAgeProject/InformationAgeProject/Dice.cs
+++ InformationAgeProject/InformationAgeProject/Dice.cs
@@ -25,6 +25,7 @@ namespace InformationAgeProject
 	{
 		private readonly Random RNG;    //Random Number Generator for use in RollDice() method
 
+		#region Dice Constructor
 		/// <summary>
 		/// Constructor for Dice 
 		/// </summary>
@@ -33,16 +34,36 @@ namespace InformationAgeProject
 			this.RNG = new Random();
 
 		}//end Dice() Constructor
+		#endregion
 
-		#region RollDice() & StaticRollDice() Methods
+		#region RollDice() & RollCustomDice() Methods
+		/// <summary>
+		/// Method for returning random value from input number of regular 6-sided dice
+		/// </summary>
+		/// <param name="numDice">Total number of regular dice to be rolled</param>
+		/// <returns>Random value from input number of regular 6-sided dice</returns>
+		public int RollDice(int numDice)
+		{
+			int rollVal = 0;    //Stored rolled value initialized to 0
+
+			for (int i = 0; i < numDice; i++)
+			{
+				rollVal += (RNG.Next(6) + 1);
+
+			}//end for loop
+
+			return rollVal;
+
+		}//end RollDice()
+
 		/// <summary>
 		/// Method for returning random value from custom number of dice with custom number of sides
 		/// </summary>
-		/// <param name="numDice">Total number of dice to be rolled</param>
+		/// <param name="numDice">Total number of custom dice to be rolled</param>
 		/// <param name="numSides">Total number of sides per die</param>
 		/// <param name="startsAt">Number of sides die starts at(RNG result wont go below this number for each die)</param>
 		/// <returns>Random value from custom number of dice with custom number of sides</returns>
-		public int RollDice(int numDice, int numSides, int startsAt)
+		public int RollCustomDice(int numDice, int numSides, int startsAt)
 		{
 			int rollVal = 0;    //Stored rolled value initialized to 0
 
@@ -62,39 +83,8 @@ namespace InformationAgeProject
 			}
 
 			return rollVal;
-		}//end RollDice()
 
-		/// <summary>
-		/// Static Method for returning random value from custom number of dice with custom number of sides
-		/// </summary>
-		/// <param name="numDice">Total number of dice to be rolled</param>
-		/// <param name="numSides">Total number of sides per die</param>
-		/// <param name="startsAt">Number of sides die starts at(RNG result wont go below this number for each die)</param>
-		/// <returns>Random value from custom number of dice with custom number of sides</returns>
-		public static int StaticRollDice(int numDice, int numSides, int startsAt)
-		{
-			Random RNG = new Random();	//RNG for die/dice roll
-			int rollVal = 0;            //Stored rolled value initialized to 0
-
-			//If an Exception occurs, the Exception message is displayed and a -1 is returned
-			try
-			{
-
-				for (int i = 0; i < numDice; i++)
-				{
-					rollVal += RNG.Next(startsAt, numSides + 1);
-
-				}//end for loop
-
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine("{0} Exception caught.", e);
-				rollVal = -1;
-			}
-
-			return rollVal;
-		}//end StaticRollDice()
+		}//end RollCustomDice()
 		#endregion
 
 	}//end Dice class
